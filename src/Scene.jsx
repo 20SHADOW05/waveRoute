cf630b5eef5261c4dbbb260dc8fcc38fb8e6bf5a
Ankababu Yadav <146865769+20SHADOW05@users.noreply.github.com>
2026-01-13 23:04:48 +0530

    rendered the earth

---
+++ src/Scene.jsx
@@ -0,0 +1,21 @@
+/*
+Credits
+Author: PatelDev (https://sketchfab.com/PatelDev)
+License: CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+Source: https://sketchfab.com/3d-models/earth-f7a76c63ff1846afb2d606e5c8369c15
+Title: Earth
+*/
+
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export default function Model(props) {
+  const { nodes, materials } = useGLTF('/scene.gltf')
+  return (
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.Object_4.geometry} material={materials['Scene_-_Root']} scale={3} />
+    </group>
+  )
+}
+
+useGLTF.preload('/scene.gltf')
